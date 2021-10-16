a13b98c713dfe84174e32a9f16c3975209dcd4ac
averger <a.verger13@gmail.com>
2021-10-16 16:29:27 +0200

    Subject adaptation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
+const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://averger:Nejowe46@cluster0.wq0w7.mongodb.net/test?retryWrites=true&w=majority',
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
